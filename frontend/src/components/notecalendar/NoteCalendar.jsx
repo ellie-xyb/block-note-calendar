808e2093dc14be446dd950a5207fed983015a764
Ellie Xu <47287801+ellie-xyb@users.noreply.github.com>
2022-01-15 09:21:43 +0900

    designed main note calendar

--- frontend/src/components/notecalendar/NoteCalendar.jsx
+++ frontend/src/components/notecalendar/NoteCalendar.jsx
@@ -1,215 +1,138 @@
 import * as React from 'react';
-import PropTypes from 'prop-types';
-import clsx from 'clsx';
-import { withStyles } from '@mui/styles';
-import { createTheme } from '@mui/material/styles';
-import TableCell from '@mui/material/TableCell';
 import Paper from '@mui/material/Paper';
-import { AutoSizer, Column, Table } from 'react-virtualized';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
 
-const styles = (theme) => ({
-  flexContainer: {
-    display: 'flex',
-    alignItems: 'center',
-    boxSizing: 'border-box',
-  },
-  table: {
-    // temporary right-to-left patch, waiting for
-    // https://github.com/bvaughn/react-virtualized/issues/454
-    '& .ReactVirtualized__Table__headerRow': {
-      ...(theme.direction === 'rtl' && {
-        paddingLeft: '0 !important',
-      }),
-      ...(theme.direction !== 'rtl' && {
-        paddingRight: undefined,
-      }),
-    },
-  },
-  tableRow: {
-    cursor: 'pointer',
-  },
-  tableRowHover: {
-    '&:hover': {
-      backgroundColor: theme.palette.grey[200],
-    },
-  },
-  tableCell: {
-    flex: 1,
-  },
-  noClick: {
-    cursor: 'initial',
-  },
-});
-
-class MuiVirtualizedTable extends React.PureComponent {
-  static defaultProps = {
-    headerHeight: 48,
-    rowHeight: 48,
-  };
-
-  getRowClassName = ({ index }) => {
-    const { classes, onRowClick } = this.props;
-
-    return clsx(classes.tableRow, classes.flexContainer, {
-      [classes.tableRowHover]: index !== -1 && onRowClick != null,
-    });
-  };
-
-  cellRenderer = ({ cellData, columnIndex }) => {
-    const { columns, classes, rowHeight, onRowClick } = this.props;
-    return (
-      <TableCell
-        component="div"
-        className={clsx(classes.tableCell, classes.flexContainer, {
-          [classes.noClick]: onRowClick == null,
-        })}
-        variant="body"
-        style={{ height: rowHeight }}
-        align={
-          (columnIndex != null && columns[columnIndex].numeric) || false
-            ? 'right'
-            : 'left'
-        }
-      >
-        {cellData}
-      </TableCell>
-    );
-  };
-
-  headerRenderer = ({ label, columnIndex }) => {
-    const { headerHeight, columns, classes } = this.props;
-
-    return (
-      <TableCell
-        component="div"
-        className={clsx(classes.tableCell, classes.flexContainer, classes.noClick)}
-        variant="head"
-        style={{ height: headerHeight }}
-        align={columns[columnIndex].numeric || false ? 'right' : 'left'}
-      >
-        <span>{label}</span>
-      </TableCell>
-    );
-  };
-
-  render() {
-    const { classes, columns, rowHeight, headerHeight, ...tableProps } = this.props;
-    return (
-      <AutoSizer>
-        {({ height, width }) => (
-          <Table
-            height={height}
-            width={width}
-            rowHeight={rowHeight}
-            gridStyle={{
-              direction: 'inherit',
-            }}
-            headerHeight={headerHeight}
-            className={classes.table}
-            {...tableProps}
-            rowClassName={this.getRowClassName}
-          >
-            {columns.map(({ dataKey, ...other }, index) => {
-              return (
-                <Column
-                  key={dataKey}
-                  headerRenderer={(headerProps) =>
-                    this.headerRenderer({
-                      ...headerProps,
-                      columnIndex: index,
-                    })
-                  }
-                  className={classes.flexContainer}
-                  cellRenderer={this.cellRenderer}
-                  dataKey={dataKey}
-                  {...other}
-                />
-              );
-            })}
-          </Table>
-        )}
-      </AutoSizer>
-    );
-  }
-}
-
-MuiVirtualizedTable.propTypes = {
-  classes: PropTypes.object.isRequired,
-  columns: PropTypes.arrayOf(
-    PropTypes.shape({
-      dataKey: PropTypes.string.isRequired,
-      label: PropTypes.string.isRequired,
-      numeric: PropTypes.bool,
-      width: PropTypes.number.isRequired,
-    }),
-  ).isRequired,
-  headerHeight: PropTypes.number,
-  onRowClick: PropTypes.func,
-  rowHeight: PropTypes.number,
-};
-
-const defaultTheme = createTheme();
-const VirtualizedTable = withStyles(styles, { defaultTheme })(MuiVirtualizedTable);
-
-// ---
-
-const sample = [
-  ['Frozen yoghurt', 159, 6.0, 24, 4.0],
-  ['Ice cream sandwich', 237, 9.0, 37, 4.3],
-  ['Eclair', 262, 16.0, 24, 6.0],
-  ['Cupcake', 305, 3.7, 67, 4.3],
-  ['Gingerbread', 356, 16.0, 49, 3.9],
+const columns = [
+  { id: 'time', label: '', width: 40, first: true  },
+  { id: 'sun', label: '16', width: 145  },
+  { id: 'mon', label: '17', width: 145  },
+  { id: 'tue', label: '18', width: 145  },
+  { id: 'wed', label: '19', width: 145  },
+  { id: 'thu', label: '20', width: 145  },
+  { id: 'fri', label: '21', width: 145  },
+  { id: 'sat', label: '22', width: 145  },
 ];
 
-function createData(id, dessert, calories, fat, carbs, protein) {
-  return { id, dessert, calories, fat, carbs, protein };
-}
+function createData(id, time) {
+    return { id, time };
+  }
 
-const rows = [];
+const rows =[
+    createData(0, '6AM'),
+    createData(1, '7AM'),
+    createData(2, '8AM'),
+    createData(3, '9AM'),
+    createData(4, '10AM'),
+    createData(5, '11AM'),
+    createData(6, '12AM'),
+    createData(7, '1PM'),
+    createData(8, '2PM'),
+    createData(9, '3PM'),
+    createData(10, '4PM'),
+    createData(11, '5PM'),
+    createData(12, '6PM'),
+];
 
-for (let i = 0; i < 200; i += 1) {
-  const randomSelection = sample[Math.floor(Math.random() * sample.length)];
-  rows.push(createData(i, ...randomSelection));
-}
+const firstRowStyle = {
+    paddingTop: '40px',
+    paddingBottom: '5px',
+    paddingLeft: '18px',
+    fontSize: '0.5em',
+    border: 'none',
+    textAlign: 'center',
+};
+
+const secondRowStyle = {
+    fontSize: '1.2em', 
+    top: 69, 
+    paddingTop: '5px', 
+    border: 'none',
+    textAlign: 'center',
+};
+
+const thirdRowStyle = { 
+    fontSize: '0,3em',  
+    top: 114, 
+    padding: 0, 
+    paddingBottom: '20px',
+    border: 'none',
+    borderBottom: '0.5px solid #E0E3E7',
+    textAlign: 'left',
+};    
 
 export default function NoteCalendar() {
   return (
-    <Paper style={{ height: '100%', width: '100%', marginLeft: '10px' }}>
-      <VirtualizedTable
-        rowCount={rows.length}
-        rowGetter={({ index }) => rows[index]}
-        columns={[
-          {
-            width: 200,
-            label: 'Dessert',
-            dataKey: 'dessert',
-          },
-          {
-            width: 120,
-            label: 'Calories\u00A0(g)',
-            dataKey: 'calories',
-            numeric: true,
-          },
-          {
-            width: 120,
-            label: 'Fat\u00A0(g)',
-            dataKey: 'fat',
-            numeric: true,
-          },
-          {
-            width: 120,
-            label: 'Carbs\u00A0(g)',
-            dataKey: 'carbs',
-            numeric: true,
-          },
-          {
-            width: 120,
-            label: 'Protein\u00A0(g)',
-            dataKey: 'protein',
-            numeric: true,
-          },
-        ]}
-      />
+    <Paper 
+    sx={{ 
+        width: '99%', 
+        overflow: 'hidden', 
+        paddingLeft: '12px',
+        }}>
+      <TableContainer sx={{ height: '90vh' }}>
+        <Table stickyHeader aria-label="sticky table">
+          <TableHead>
+            <TableRow>
+              <TableCell style={{ border: 'none' }}></TableCell>
+              <TableCell style={firstRowStyle}>SUN</TableCell>
+              <TableCell style={firstRowStyle}>MON</TableCell>
+              <TableCell style={firstRowStyle}>TUE</TableCell>
+              <TableCell style={firstRowStyle}>WED</TableCell>
+              <TableCell style={firstRowStyle}>THU</TableCell>
+              <TableCell style={firstRowStyle}>FRI</TableCell>
+              <TableCell style={firstRowStyle}>SAT</TableCell>
+            </TableRow>
+            <TableRow>
+              {columns.map((column) => (
+                <TableCell
+                  key={column.id}
+                  style={secondRowStyle}
+                >
+                  {column.label}
+                </TableCell>
+              ))}
+            </TableRow>
+            <TableRow>
+                <TableCell 
+                  style={thirdRowStyle} 
+                  colSpan={8} 
+                  align='left'
+                >
+                  GMT+09
+                </TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows
+              .map((row) => {
+                return (
+                  <TableRow hover role="checkbox" tabIndex={-1} key={row.id}>
+                    {columns.map((column) => {
+                      const value = row[column.id];
+                      return (
+                        <TableCell 
+                          key={column.id} 
+                          align={column.first ? 'center' : 'left'} 
+                         width={column.width} 
+                          overFlow='hidden'
+                          style={{
+                            borderRight: '0.5px solid #E0E3E7',
+                          }}
+                        >
+                          {value}
+                        </TableCell>
+                      );
+                    })}
+                  </TableRow>
+                );
+              })}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </Paper>
   );
 }
