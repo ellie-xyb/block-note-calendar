9203c042b79682aab67e3230a3a2c93a8f09e694
Ellie Xu <47287801+ellie-xyb@users.noreply.github.com>
2022-01-26 18:14:21 +0900

    change the task form content

--- frontend/src/components/taskbar/TaskForm.jsx
+++ frontend/src/components/taskbar/TaskForm.jsx
@@ -2,9 +2,14 @@ import * as React from 'react';
 import FormControl from '@mui/material/FormControl';
 import TextField from '@mui/material/TextField';
 import Box from '@mui/material/Box';
-import TaskDateTimePicker from './TaskDateTimePicker';
+// import TaskDateTimePicker from './TaskDateTimePicker';
+import AdapterDateFns from '@mui/lab/AdapterDateFns';
+import LocalizationProvider from '@mui/lab/LocalizationProvider';
+import DatePicker from '@mui/lab/DatePicker';
+import DesktopDatePicker from '@mui/lab/DesktopDatePicker';
 
 export default function TaskForm() {
+  const [value, setValue] = React.useState(new Date());
   return (
     <Box
       component="form"
@@ -21,13 +26,33 @@ export default function TaskForm() {
             fullWidth
             variant="standard"
           />
-          <TaskDateTimePicker />
+          <LocalizationProvider dateAdapter={AdapterDateFns}>
+            <DesktopDatePicker
+            //   label="Choose a day"
+              value={value}
+              minDate={new Date('2022-01-01')}
+              onChange={(newValue) => {
+                setValue(newValue);
+              }}
+            //   renderInput={(params) => <TextField {...params} />}
+              renderInput={({ inputRef, inputProps, InputProps }) => (
+                <Box sx={{ 
+                    width: '45%', 
+                    display: 'flex', 
+                    alignItems: 'center', 
+                    }}>
+                  {InputProps?.endAdornment}
+                  <input ref={inputRef} {...inputProps} style={{ marginLeft: '8px' }} />
+                </Box>
+              )}
+            />
+          </LocalizationProvider>
           <TextField
             label="Add description"
             variant="filled"
             multiline
             rows={6}
-        />
+          />
         </FormControl>
     </Box>
   );
