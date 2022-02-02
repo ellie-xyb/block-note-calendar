841ce75f83b837f27add0b01554e0fb1b82c93f9
Ellie Xu <47287801+ellie-xyb@users.noreply.github.com>
2022-02-02 19:20:45 +0900

    change task detail dialog style

--- frontend/src/components/tasks/TaskShow.jsx
+++ frontend/src/components/tasks/TaskShow.jsx
@@ -9,10 +9,14 @@ import DialogTitle from '@mui/material/DialogTitle';
 export default function TaskShow(props) {
   return (
       <Dialog
+        hideBackdrop={true}
         open={props.taskOpen}
         onClose={props.handleTaskClose}
         aria-labelledby="task-detail"
         aria-describedby="task-detail"
+        style={{
+            boxShadow: '0px 1px 9px -7px rgb(0 0 0 / 20%), 0px 9px 20px 3px rgb(0 0 0 / 14%), 0px 0px 46px 8px rgb(0 0 0 / 12%)',
+        }}
       >
         <DialogTitle id="task-title">
           {"task title"}
