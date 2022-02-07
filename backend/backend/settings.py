7ea0172a8a21be0cfb39a5c5b96b19cfaf0788b7
Ellie Xu <47287801+ellie-xyb@users.noreply.github.com>
2022-02-08 04:04:20 +0900

    rename app name

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'users.apps.UsersConfig',
+    'api.apps.ApiConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
