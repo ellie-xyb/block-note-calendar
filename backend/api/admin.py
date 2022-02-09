375b64d9105eeff8cee4832e71e69d26c52eed09
Ellie Xu <47287801+ellie-xyb@users.noreply.github.com>
2022-02-09 20:51:58 +0900

    register cell model to admin site

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
-from .models import Task
-
-# admin.site.register(Task)
+from .models import Task, Cell
 
 
 @admin.register(Task)
 class TaskModel(admin.ModelAdmin):
     list_display = ('id', 'title', 'content', 'user')
+
+
+@admin.register(Cell)
+class CellModel(admin.ModelAdmin):
+    list_display = ('id', 'start_datetime', 'end_datetime', 'task')
